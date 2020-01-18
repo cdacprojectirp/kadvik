2c7fb736175a6f6bf5fef9e5c7276b59061475cc
Akshay <akshay0x@gmail.com>
2020-01-18 20:33:03 +0530

    trial

--- irp/src/main/java/com/cdac/irp/controller/QuizController.java
+++ irp/src/main/java/com/cdac/irp/controller/QuizController.java
@@ -2,8 +2,11 @@ package com.cdac.irp.controller;
 
 import java.util.List;
 
+import javax.annotation.PostConstruct;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,12 +21,16 @@ public class QuizController {
 	@Autowired
 	private IQuestionService service;
 
+	@PostConstruct
 	public void init() {
 		System.out.println("in init " + service);
 	}
 
+	@GetMapping("/quiz")
 	public List<Question> getExamQuestions() {
 		return service.getTenQuestionList();
 	}
+	
+	
 
 }
--- irp/src/main/java/com/cdac/irp/service/QuestionServiceImpl.java
+++ irp/src/main/java/com/cdac/irp/service/QuestionServiceImpl.java
@@ -3,10 +3,12 @@ package com.cdac.irp.service;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.cdac.irp.dao.IQuestionDao;
 import com.cdac.irp.pojos.Question;
 
+@Service
 public class QuestionServiceImpl implements IQuestionService {
 
 	@Autowired
