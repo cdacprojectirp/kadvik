ffad78639c4973586edfb7be47421ec3b8d7c62d
devanknassa <devanknassa@gmail.com>
2020-01-24 20:55:06 +0530

    devank Notice Board Added

---
+++ irp/src/main/java/com/cdac/irp/dao/INoticeBoardDao.java
@@ -0,0 +1,9 @@
+package com.cdac.irp.dao;
+
+import java.util.List;
+import com.cdac.irp.pojos.NoticeBoard;
+
+public interface INoticeBoardDao {
+	
+		List<NoticeBoard> getNoticeBoard();
+}
---
+++ irp/src/main/java/com/cdac/irp/service/NoticeBoardServiceImpl.java
@@ -0,0 +1,28 @@
+package com.cdac.irp.service;
+
+
+import java.util.List;
+
+import javax.transaction.Transactional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.cdac.irp.dao.INoticeBoardDao;
+import com.cdac.irp.pojos.NoticeBoard;
+
+@Service
+@Transactional
+public class NoticeBoardServiceImpl implements INoticeBoard {
+	
+	@Autowired
+	private INoticeBoardDao dao;
+	
+	@Override
+	public List<NoticeBoard> getNoticeBoard(){
+		System.out.println("in noticeboard service");
+		return dao.getNoticeBoard();
+	}
+
+
+}
